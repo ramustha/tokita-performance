293d596d749fcc13eee5a3eb236013ff79cab607
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2024-07-23 14:23:53 +0700

    use ramping vue

--- src/auth.js
+++ src/auth.js
@@ -1,20 +1,22 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import {sleep} from 'k6';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 10,
-  // A string specifying the total duration of the test run.
-  duration: '10s',
+  scenarios: {
+    auth: {
+      executor: 'ramping-vus',
+      exec: 'auth',
+      startVUs: 0,
+      stages: [
+        { duration: '20s', target: '20'},
+        { duration: '50s', target: '20'},
+      ]
+    }
+  }
 };
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
-//
-export default function() {
-  console.log('auth test')
+export function auth() {
+  console.log('perform auth scenario')
   http.get('https://test.k6.io');
   sleep(1);
 }
